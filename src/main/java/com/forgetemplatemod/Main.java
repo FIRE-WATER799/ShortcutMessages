5d0bcb3fd0f36b814befe6ee2d9f49c72c429d80
FIRE-WATER799 <haywood2008918@outlook.com>
2023-09-08 17:04:20 -0500

    Update Main.java

--- src/main/java/com/forgetemplatemod/Main.java
+++ src/main/java/com/forgetemplatemod/Main.java
@@ -2,51 +2,10 @@ package com.forgetemplatemod;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.WebSocketAdapter;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.websocket.server.WebSocketHandler;
-import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
-
-
-public class MyWebSocketHandler extends WebSocketAdapter {
-    @Override
-    public void onWebSocketConnect(Session sess) {
-        super.onWebSocketConnect(sess);
-        // Handle authentication here if needed
-    }
-
-    @Override
-    public void onWebSocketText(String message) {
-        // Forward the message to Minecraft chat
-        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(message);
-    }
-}
-
-public class MyWebSocketServer {
-    private Server server;
-
-    public MyWebSocketServer(int port) {
-        server = new Server(port);
-        
-        WebSocketHandler wsHandler = new WebSocketHandler() {
-            @Override
-            public void configure(WebSocketServletFactory factory) {
-                factory.register(MyWebSocketHandler.class);
-            }
-        };
-
-        server.setHandler(wsHandler);
-    }
-
-    public void start() throws Exception {
-        server.start();
-        server.join();
-    }
-}
+import com.forgemodtemplate.MyWebSocketServer
 
 @Mod(modid = "ShortcutMessages", version = "1.0")
-public class ShortcutMessages {
+public class Main {
 
     private static MyWebSocketServer webSocketServer;
 
