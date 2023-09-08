0999438225ae4e5ad3d5c5b88bfe1706575de184
FIRE-WATER799 <haywood2008918@outlook.com>
2023-09-08 17:06:01 -0500

    Create MyWebSocketHandler.java

---
+++ src/main/java/com/forgetemplatemod/MyWebSocketHandler.java
@@ -0,0 +1,16 @@
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+
+public class MyWebSocketHandler extends WebSocketAdapter {
+    @Override
+    public void onWebSocketConnect(Session sess) {
+        super.onWebSocketConnect(sess);
+        // Handle authentication here if needed
+    }
+
+    @Override
+    public void onWebSocketText(String message) {
+        // Forward the message to Minecraft chat
+        Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(message);
+    }
+}
