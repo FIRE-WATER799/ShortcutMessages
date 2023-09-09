5d1babc73a38091084857cd622de7e1b9945659e
FIRE-WATER799 <haywood2008918@outlook.com>
2023-09-08 19:38:11 -0500

    Update Main.java

--- src/main/java/com/forgetemplatemod/Main.java
+++ src/main/java/com/forgetemplatemod/Main.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraft.util.text.ITextComponent; 
 import net.minecraft.util.text.TextComponentString;
 import com.forgetemplatemod.MyWebSocketServer;
+import net.minecraft.client.Minecraft;
 
 @Mod(modid = "ShortcutMessages", version = "1.0")
 public class Main {
